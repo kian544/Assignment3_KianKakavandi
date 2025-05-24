7976faef8882bc844ea915e1c025105b8955faa3
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-24 02:04:57 -0500

    Created PreCreation junit test

--- src/test/java/EagerSingletonTest.java
+++ src/test/java/EagerSingletonTest.java
@@ -1,1 +1,13 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class EagerSingletonTest {
+    @Test
+    public void TestPreCreation(){
+        for(int i=0;i<3;i++){
+            EagerSingleton e=EagerSingleton.getInstance(i);
+            assertEquals(i, e.getId());
+        }
+    }
+}
