5d72bca41cc896746534aad4700b754d432e28c5
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-24 02:24:38 -0500

    Added instances as required test, (testAsRequired)

--- src/test/java/LazySingletonTest.java
+++ src/test/java/LazySingletonTest.java
@@ -2,4 +2,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LazySingletonTest {
+
+    @Test
+    public void testAsRequired(){ //checking to see if it works and creates
+                                  // the exact instance ive asked it to
+        LazySingleton l1=LazySingleton.getInstance(0);
+        assertEquals(0,l1.getId());
+
+        LazySingleton l2=LazySingleton.getInstance(1);
+        assertEquals(1,l2.getId());
+    }
 }
