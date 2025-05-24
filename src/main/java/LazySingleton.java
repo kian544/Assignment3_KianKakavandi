1f3bffd3291a201ec98746489ba80eac365929a3
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-24 02:18:10 -0500

    Completed Lazysingleton and added javadoc comments

---
+++ src/main/java/LazySingleton.java
@@ -0,0 +1,42 @@
+/**
+ * Creating instances when needed, non-instant
+ */
+public class LazySingleton {
+
+    private static LazySingleton[]  instances = new LazySingleton[3];
+
+    private int id;
+
+    /**
+     *
+     * @param id 0,1,2
+     */
+    private LazySingleton(int id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return id of LazySingleton
+     */
+    public int getId() {
+        return id;
+    }
+
+
+    /**
+     *
+     * @param index of instance 0,1,2
+     * @return the LazySingleton
+     * @throw OOB exception
+     */
+    private static LazySingleton getInstance(int index){
+        if(index<0 || index>2){
+            throw new IllegalArgumentException("index out of bounds, must be between 0 and 2");
+        }
+        if(instances[index]==null){
+            instances[index] = new LazySingleton(index);
+        }
+        return instances[index];
+    }
+}
