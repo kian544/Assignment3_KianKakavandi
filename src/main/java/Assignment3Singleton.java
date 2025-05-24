6aff05634015a698623c3cce6db67c249603fc2b
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-24 02:10:31 -0500

    Uncommented LazySingleton

--- src/main/java/Assignment3Singleton.java
+++ src/main/java/Assignment3Singleton.java
@@ -9,12 +9,12 @@ public class Assignment3Singleton {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {}
         }
-        /**for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             LazySingleton s = LazySingleton.getInstance(r.nextInt(3));
             System.out.println("Retrieved lazy singleton " + s.getId());
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {}
-        }  **/
+        }
     }
 } 
