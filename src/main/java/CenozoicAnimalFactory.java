cdf7ca9d61dc5eb424595304ef3d69249e65ad81
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-24 19:58:49 -0500

    Added wooly rhino and action

--- src/main/java/CenozoicAnimalFactory.java
+++ src/main/java/CenozoicAnimalFactory.java
@@ -5,4 +5,11 @@ public class CenozoicAnimalFactory implements AnimalAbstractFactory {
     public String getEra(){
         return "We are in the Cenozoic era!";
     }
+
 }
+
+class WollyRhino implements LandAnimal {
+    public String getName(){ return "Wolly Rhino"; }
+    public String getWalking(){return "stomping!";}
+}
+
