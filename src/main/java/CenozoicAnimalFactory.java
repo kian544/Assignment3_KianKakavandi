b3aa0f980ed45b39d40b009ab8edffeda37b8c55
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-24 19:55:11 -0500

    Created a factory for the specific CenozoicAnimalFactory and added the era pull

---
+++ src/main/java/CenozoicAnimalFactory.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.Arrays;
+
+public class CenozoicAnimalFactory implements AnimalAbstractFactory {
+    public String getEra(){
+        return "We are in the Cenozoic era!";
+    }
+}
